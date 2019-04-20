37c4446b1d4ca25e8b93a85dc6eef1c49000b091
Christian Baros <ctbaros@gmail.com>
2019-04-20 17:41:28 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+ for (let i = 0 ; i < 25; i++) {
+ if (i === 1) {
+   array.push ('I am 1 strange loop.')
+   } else {
+   array.push ('I am 24 strange loops.')
+ }
+ }
+ return array;
+}
+
+function whileLoop(n) {
+  let countdown = n
+
+while (countdown > 0) {
+  console.log(--countdown)
+}
+return "done";
+}
+
+function doWhileLoop (array) {
+  function maybeTrue() {
+    return math.random <= .5
+  }
+  
+do {
+   maybeTrue();
+} while (array.length 
+  return array;
+}
